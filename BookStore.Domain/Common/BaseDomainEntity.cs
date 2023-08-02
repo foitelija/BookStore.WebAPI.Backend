0e71ba5c94de7b534aab5640aaec94cde8469646
Foitelija <foitelija777@gmail.com>
2023-08-02 14:55:21 +0300

    Добавьте файлы проекта.

---
+++ BookStore.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore.Domain.Common
+{
+    public abstract class BaseDomainEntity
+    {
+        public int Id { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
+    }
+}
